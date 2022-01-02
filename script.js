1b7ec31c222991df855deebef3c206bfac4cc3ad
lrosenqv <kimle.rosenqvist@gmail.com>
2022-01-02 02:39:41 +0100

    elements in module

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-// -- DOCUMENT BASE -- /
-let root = document.getElementById("root")
-let header = document.createElement("header")
-let main = document.createElement("main")
-let footer = document.createElement("footer")
-footer.innerHTML = "This is a footer"
-
+import { root, header, main, footer } from "./modules/elements.mjs";
 root.append(header, main, footer)
 
-// -- LOGIN FORM --/
-let loginForm = document.createElement("div")
-loginForm.id = "loginForm"
+import { loginForm, userNameInput, passwordInput, loginBtn, errorMsg } from "./modules/elements.mjs";
+loginForm.append(userNameInput, passwordInput, loginBtn, errorMsg )
 
-let userNameInput = document.createElement ("input")
-userNameInput.placeholder = "Username"
-userNameInput.type = "text"
-userNameInput.required = true
-
-let passwordInput = document.createElement ("input")
-passwordInput.placeholder = "Password"
-passwordInput.type = "password"
-passwordInput.required = true
-
-let loginBtn = document.createElement ("button")
-loginBtn.innerHTML = "Log In"
-loginBtn.id = "loginBtn"
-
-let errorMsg = document.createElement("div")
-errorMsg.id = "errorMsg"
-errorMsg.innerText = "Wrong username or password"
-
-loginForm.append(userNameInput, passwordInput, loginBtn)
-
-// -- BOX THAT REPLACES LOGIN FORM WHEN LOGGED IN --/
-let onlineBox = document.createElement("div")
-onlineBox.id = "onlineBox"
-onlineBox.insertAdjacentHTML("afterbegin", "<p>You are now logged in as admin</p>")
-
-let logoutBtn = document.createElement("button")
-logoutBtn.innerHTML = "Log out"
-logoutBtn.id = "logoutBtn"
-
-let viewToggle = document.createElement("button")
-viewToggle.id = "viewToggle"
-
+import { onlineBox, logoutBtn, viewToggle } from "./modules/elements.mjs";
 onlineBox.append(viewToggle, logoutBtn)
 
-// -- CONTENT --/ 
-let contentContainer = document.createElement("div")
-contentContainer.id = "contentContainer"
-let h1 = document.createElement("h1")
-let p = document.createElement("p")
+import { contentContainer, h1, p } from "./modules/elements.mjs";
 contentContainer.append(h1, p)
+
+import { editContentContainer, newTitle, newText, saveContentBtn } from "./modules/elements.mjs";
+editContentContainer.append(newTitle, newText, saveContentBtn)
+
+import { editThemeContainer, newFontTitle, newTitleColour, newFontText, newTextColour, newAccentColour, newContrastColour, saveEditsBtn, saveThemeBtn, themesDiv, themeList } from "./modules/elements.mjs";
+editThemeContainer.append(newFontTitle, newTitleColour, newFontText, newTextColour, newAccentColour, newContrastColour, saveEditsBtn, saveThemeBtn, themesDiv)
+
+newFontTitle.insertAdjacentHTML("beforebegin", "<h3>Edit Title Font</h3>")
+newTitle.insertAdjacentHTML("beforebegin", "<h3>Edit Title</h3>")
+newFontText.insertAdjacentHTML("beforebegin", "<h3>Edit Text Font</h3>")
+newText.insertAdjacentHTML("beforebegin", "<h3 id='newTextH3'>Edit Text</h3>")
+newTitleColour.insertAdjacentHTML("beforebegin", "<h3>Title Colour</h3>")
+newTextColour.insertAdjacentHTML("beforebegin", "<h3>Text Colour</h3>")
+newAccentColour.insertAdjacentHTML("beforebegin", "<h3>Accent Colour</h3>")
+newContrastColour.insertAdjacentHTML("beforebegin", "<h3>Accent-text Colour</h3>")
+
+import { saveThemeContainer, newThemeName, saveThemeBtn2 } from "./modules/elements.mjs";
+saveThemeContainer.append(newThemeName, saveThemeBtn2)
+
+// -- ARRAY WITH FONTS TO CHOOSE FROM --/
+let fonts = [
+    "Helvetica",
+    "Arial",
+    "Times New Roman",
+    "Courier New"
+]
+
+// -- CREATING SELECT ELEMENTS FOR SELECTING FONT --/
+fonts.forEach((font) => {
+    let aFont = document.createElement("option")
+    aFont.innerText = font
+    newFontTitle.append(aFont)
+})
+
+fonts.forEach((font) => {
+    let aFont = document.createElement("option")
+    aFont.innerText = font
+    newFontText.append(aFont)
+})
+
+// ------------------------------------------------ CODE STARTS HERE ------------------------------------------------ /
 
 window.addEventListener('load', initPage)
 
-let onlineUser = []
-
-checkTheme()
-
 // -- WHEN LOADING PAGE --/
 function initPage() {
+    checkTheme()
     let onlineUser = JSON.parse(localStorage.getItem("onlineUser"))
     let activeTheme = JSON.parse(localStorage.getItem("activeTheme"))
     setTheme(activeTheme)
@@ -111,7 +107,6 @@ async function checkTheme(){
     }
 }
 
-getThemes()
 // -- SET THEMES OF JSON IN LS --/
 async function getThemes(){
     let themesList = JSON.parse(localStorage.getItem("themes"))
@@ -134,11 +129,7 @@ function getUsersFromLS() {
     }
 }
 
-function clearErrorMsg(){
-    errorMsg.remove()
-}
 
-let interval = setInterval
 // -- LOG IN & USER VALIDATION --/
 loginBtn.addEventListener('click', validateUser)
 function validateUser() {
@@ -150,7 +141,7 @@ function validateUser() {
     let timeout;
 
     if(!userOk || !passwordOk) {
-        loginForm.append(errorMsg)
+        header.append(errorMsg)
         timeout = setTimeout(clearErrorMsg, 2000)
     }
 
@@ -159,6 +150,11 @@ function validateUser() {
         localStorage.setItem("onlineUser", JSON.stringify(onlineUser))
         adminPage()
     } 
+}
+
+// -- CLEAR ERROR MESSAGE WHEN LOGIN DETAILS IS INCORRECT --/
+function clearErrorMsg(){
+    errorMsg.remove()
 }
 
 // -- Page for logged in users -- /
@@ -193,108 +189,6 @@ Coloris({
     el: '.Coloris'
 });
 
-
-// -- CREATING EDIT CONTENT FORM ELEMENTS -- / 
-let editContentContainer = document.createElement("div")
-editContentContainer.id = "editContentContainer"
-editContentContainer.innerHTML = "<h2>Change Content</h2>"
-
-let newTitle = document.createElement("input")
-newTitle.id = "newTitle"
-newTitle.type = "text"
-
-let newText = document.createElement("textarea")
-newText.cols = 50
-newText.rows = 10
-newText.id = "newText"
-
-let saveContentBtn = document.createElement("button")
-saveContentBtn.innerText = "Save Content"
-saveContentBtn.id = "saveContentBtn"
-
-// -- CREATING EDIT THEME ETC. FORM ELEMENTS -- /
-let editThemeContainer = document.createElement("div")
-editThemeContainer.id = "editThemeContainer"
-editThemeContainer.innerHTML = "<h2>Customize Theme</h2>"
-  
-let newTitleColour = document.createElement("input")
-newTitleColour.className = "Coloris"
-
-let newTextColour = document.createElement("input")
-newTextColour.className = "Coloris"
-
-let newAccentColour = document.createElement("input")
-newAccentColour.className = "Coloris"
-
-let newContrastColour = document.createElement("input")
-newContrastColour.className = "Coloris"
-
-let saveEditsBtn = document.createElement("button")
-saveEditsBtn.innerText = "Save Edits"
-saveEditsBtn.id = "saveEditsBtn"
-
-let saveThemeBtn = document.createElement("button")
-saveThemeBtn.innerText = "Save as Theme"
-saveThemeBtn.id = "saveThemeBtn"
-
-let themesDiv = document.createElement("div")
-themesDiv.id = "themesDiv"
-
-let themeList = document.createElement("ul")
-themeList.id = "themeList"
-themeList.innerText = ""
-
-// -- Change font for title -- /
-let newFontTitle = document.createElement("select")
-let newFontText = document.createElement("select")
-
-// -- ARRAY WITH FONTS TO CHOOSE FROM --/
-let fonts = [
-    "Helvetica",
-    "Arial",
-    "Times New Roman",
-    "Courier New"
-]
-
-// -- CREATING SELECT ELEMENTS FOR SELECTING FONT --/
-fonts.forEach((font) => {
-    let aFont = document.createElement("option")
-    aFont.innerText = font
-    newFontTitle.append(aFont)
-})
-
-fonts.forEach((font) => {
-    let aFont = document.createElement("option")
-    aFont.innerText = font
-    newFontText.append(aFont)
-})
-
-// -- APPEND RIGHT ELEMENTS TO RIGHT DIV --/
-editContentContainer.append(newTitle, newText, saveContentBtn)
-editThemeContainer.append(newFontTitle, newTitleColour, newFontText, newTextColour, newAccentColour, newContrastColour, saveEditsBtn, saveThemeBtn, themesDiv)
-
-newFontTitle.insertAdjacentHTML("beforebegin", "<h3>Edit Title Font</h3>")
-newTitle.insertAdjacentHTML("beforebegin", "<h3>Edit Title</h3>")
-newFontText.insertAdjacentHTML("beforebegin", "<h3>Edit Text Font</h3>")
-newText.insertAdjacentHTML("beforebegin", "<h3 id='newTextH3'>Edit Text</h3>")
-newTitleColour.insertAdjacentHTML("beforebegin", "<h3>Title Colour</h3>")
-newTextColour.insertAdjacentHTML("beforebegin", "<h3>Text Colour</h3>")
-newAccentColour.insertAdjacentHTML("beforebegin", "<h3>Accent Colour</h3>")
-newContrastColour.insertAdjacentHTML("beforebegin", "<h3>Accent-text Colour</h3>")
-
-//-- Pop Up save new Theme --/
-let saveThemeContainer = document.createElement("div")
-saveThemeContainer.id = "saveThemeContainer"
-saveThemeContainer.insertAdjacentHTML("afterbegin", "<h4>Add Theme Name</h4>")
-
-let newThemeName = document.createElement("input")
-newThemeName.id = "newThemeName"
-
-let saveThemeBtn2 = document.createElement("button")
-saveThemeBtn2.innerText = "Save Theme"
-saveThemeBtn2.id = "saveThemeBtn2"
-
-saveThemeContainer.append(newThemeName, saveThemeBtn2)
 
 // -- ADMIN VIEW --/
 function adminView(){
@@ -348,11 +242,6 @@ function openSaveThemeContainer(){
 
 function saveTheme(){
     let collectedThemes = JSON.parse(localStorage.getItem("themes"))
-    /*window.addEventListener("click", (evt) => {
-        if(evt.target.id != "saveThemeBtn2"){
-            saveThemeContainer.remove()
-        }
-    })*/
 
     let lastItem = collectedThemes[collectedThemes.length - 1]
     let newID = lastItem.themeID + 1
